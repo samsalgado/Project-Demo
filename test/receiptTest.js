73fa387c01eba18f32ec8a33190d6ee7c40f61a3
cpadua912 <cristian.padua912@gmail.com>
2021-08-04 23:57:12 -0400

    Fixed test. We need to add more code to the receipt contract

--- test/receiptTest.js
+++ test/receiptTest.js
@@ -1,11 +1,27 @@
-const Receipt = artifacts.require("Receipt")
-const truffleAssert = require('truffle-assertions')
-contract("Receipt", accounts => {
-    //The contract should generate a receipt ID and display the receipt.
-    it("should throw an error if there is not a receipt generated", async() => {
-        let receipt = await Receipt.deployed()
-        await truffleAssert.passes(
-            receipt.issueReceipt(0, "Shipper A", "Cosignee B", "Destination X", 545, 5, 455, 05464645, 4546421, 02/24/2021)
-        )
-    })
-})
+const Receipt = artifacts.require("Receipt");
+contract("Receipt", (accounts) => {
+  //The contract should generate a receipt ID and display the receipt.
+  let receipt;
+  const admin = accounts[0];
+
+  beforeEach(async () => {
+    receipt = await Receipt.new();
+  });
+
+  // We need to add more to the receipt smart contract
+  it("should issue a receipt", async () => {
+    const date = new Date().toJSON().slice(0, 10);
+    await receipt.issueReceipt(
+      0,
+      "Hanjin",
+      "Mr. Bill",
+      "New York, NY",
+      3,
+      25,
+      75,
+      0,
+      12,
+      parseInt(date)
+    );
+  });
+});
