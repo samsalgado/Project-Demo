57a04cda11c59f585768bf92165b1efc1773ca1a
cpadua912 <cristian.padua912@gmail.com>
2021-08-04 20:22:06 -0400

    Fixed receipt contract

--- contracts/Receipt.sol
+++ contracts/Receipt.sol
@@ -3,86 +3,107 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Receipt is ReentrancyGuard {
-struct Warehouse_Receipt{
-    uint256 id;
-    string shipper;
-    string cosignee;
-    string destination;
-    uint64 unit_Weight;
-    uint64 number_of_packages;
-    uint64 total_weight;
-    uint256 item_id;
-    uint256 package_id;
-    uint256 date;
+    struct warehouseReceipt {
+        uint256 id;
+        string shipper;
+        string cosignee;
+        string destination;
+        uint64 unitWeight;
+        uint64 numberOfPackages;
+        uint64 totalWeight;
+        uint256 itemId;
+        uint256 packageId;
+        uint256 date;
+    }
+
+    warehouseReceipt[] public receipts;
+
+    event ReceiptIssued(
+        uint256 ReceiptId,
+        string shipper,
+        string cosignee,
+        string destination,
+        uint64 unitWeight,
+        uint64 numberOfPackages,
+        uint64 totalWeight,
+        uint256 itemId,
+        uint256 packageId,
+        uint256 date
+    );
+
+    function issueReceipt(
+        uint256 _id,
+        string memory _shipper,
+        string memory _cosignee,
+        string memory _destination,
+        uint64 _unitWeight,
+        uint64 _numberOfPackages,
+        uint64 _totalWeight,
+        uint64 _itemId,
+        uint256 _packageId,
+        uint256 _date
+    ) external returns (uint256) {
+        warehouseReceipt memory _warehouseReceipt = warehouseReceipt(
+            _id,
+            _shipper,
+            _cosignee,
+            _destination,
+            _unitWeight,
+            _numberOfPackages,
+            _totalWeight,
+            _itemId,
+            _packageId,
+            _date
+        );
+        receipts.push(_warehouseReceipt);
+        uint256 receiptId = receipts.length - 1;
+        emit ReceiptIssued(
+            receiptId,
+            _shipper,
+            _cosignee,
+            _destination,
+            _unitWeight,
+            _numberOfPackages,
+            _totalWeight,
+            _itemId,
+            _packageId,
+            _date
+        );
+        return receiptId;
+    }
+
+    // Waaaayy too many return values
+    // Too many arguments, local variables, and return values will break the compiler
+    // Not necessary if we have structs
+    // Also, never name the return values; only declare their variable typesz
+    function showReceipts(uint256 _id)
+        external
+        view
+        returns (
+            uint256,
+            string memory,
+            string memory,
+            string memory,
+            uint64,
+            uint64,
+            uint64,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        warehouseReceipt memory receipt = receipts[_id];
+        return (
+            receipt.id,
+            receipt.shipper,
+            receipt.cosignee,
+            receipt.destination,
+            receipt.unitWeight,
+            receipt.numberOfPackages,
+            receipt.totalWeight,
+            receipt.itemId,
+            receipt.packageId,
+            receipt.date
+        );
+    }
 }
-Warehouse_Receipt[] private receipts;
-event ReceiptIssued(
-uint256 ReceiptId,
-string shipper,
-string cosignee,
-string destination,
-uint64 unit_Weight,
-uint64 number_of_packages,
-uint64 total_weight,
-uint256 item_id,
-uint256 package_id,
-uint256 date
-);
-
-
-function issueReceipt(uint256 _id,
-string memory _shipper,
-string memory _cosignee,
-string memory _destination,
-uint64 _unit_Weigth,
-uint64 _number_of_packages,
-uint64 _total_weight,
-uint64 _item_id,
-uint256 _package_id,
-uint256 _date) external returns (uint256) {
-    Warehouse_Receipt memory _Warehouse_Receipt = Warehouse_Receipt({
-        id: uint256 (_id),
-        shipper: string(_shipper),
-        cosignee: string(_cosignee),
-        destination: string(_destination),
-        unit_Weight: uint64(_unit_Weigth),
-        number_of_packages: uint64(_number_of_packages),
-        total_weight: uint64(_total_weight),
-        item_id: uint64(_item_id),
-        package_id: uint256(_package_id),
-        date: uint256(_date)
-    });
-    receipts.push(_Warehouse_Receipt);
-    uint256 receiptId = receipts.length-1;
-    emit ReceiptIssued( receiptId, _shipper, _cosignee, _destination, _unit_Weigth, _number_of_packages, _total_weight, _item_id, _package_id, _date);
-   return receiptId; 
-}
-function showcaseReceipts(uint256 _id) external view returns(
-uint256 id,
-    string memory shipper,
-    string memory cosignee,
-    string memory destination,
-    uint64 unit_Weight,
-    uint64 number_of_packages,
-    uint64 total_weight,
-    uint256 item_id,
-    uint256 package_id,
-    uint256 date
-) {
-    return (
-        receipts[_id].id,
-        receipts[_id].shipper,
-        receipts[_id].cosignee,
-        receipts[_id].destination,
-        receipts[_id].unit_Weight,
-        receipts[_id].number_of_packages,
-        receipts[_id].total_weight,
-        receipts[_id].item_id,
-        receipts[_id].package_id,
-        receipts[_id].date
-);
-}
-
-
-}
-
